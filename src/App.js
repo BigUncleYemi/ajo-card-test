4655ef877525b032ebedc05d5000f872444bef5e
BigUncleYemi <olayemioyebanji911@gmail.com>
2019-03-29 19:14:11 +0100

    Add: header and list of data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React, { Component } from 'react';
-import * as images from './assets/images'
+// import * as images from './assets/images'
 import './assets/css/App.css';
+import Axios from 'axios'
+
+const date =  new Date();
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      data: ''
+    }
+  }
+
+  handleFetch = async event => {
+  const response = await Axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://traf.nibss-plc.com.ng:7443/traf/ajax?command=website&action=detail&order=loadPOS&clientCode=NIBSS&txnSubCat=ALL`);
+  this.setState({
+    data: response.data.msg
+  })
+};
+
+  componentDidMount(){
+    this.handleFetch()
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={images.logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <>
+        <header>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-sm-5 py-2">
+            <a className="navbar-brand text-uppercase" href="/#">Nibss</a>
+            <button className="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>{console.log(Object.entries(this.state.data))}
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="nav navbar-nav menu_nav ml-auto mr-3 mt-3 mt-lg-0">
+                <li className="nav-item active mx-2"><a className="nav-link" href="/#">{date.toLocaleTimeString()}</a></li>
+                <li className="nav-item active mx-2"><a className="nav-link" href="/#">{date.toDateString()}</a></li>
+              </ul>
+            </div>
+          </nav>
         </header>
-      </div>
+        <section className="container-fluid">
+          <div className="row">
+            <div className="col-sm-4">
+              <div className="card my-4">
+                <div className="card-body my-auto">
+                  <h2 className="card-title display-4">{this.state.data.entityName}</h2>
+                  <h5 className="card-text mb-3">{this.state.data.txnCategory} Transaction</h5>
+                  <p>Range Failure Rate Outward: {this.state.data.rangeFailureRateOutward}</p>
+                  <p>Today Failure Rate Outward: {this.state.data.todayFailureRateOutward}</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-sm-4">
+              <div className="my-4 border-bottom">
+                <span className="list-group-item d-flex justify-content-between align-items-center bg-dark text-white position-sticky">Today</span>
+                <ul className="list-group">
+                  {Object.entries(this.state.data).filter( item => item[0].includes('today')).map((item, index)=> (
+                    <li key={index} className="list-group-item d-flex justify-content-between align-items-center">{item[0].toString()} <span className="badge">{item[1].toString()}</span> </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="col-sm-4">
+              <div className="my-4 border-bottom">
+                <span className="list-group-item d-flex justify-content-between align-items-center bg-dark text-white">Current Time Range</span>
+                <ul className="list-group">
+                  {Object.entries(this.state.data).filter( item => item[0].includes('range')).map((item, index)=> (
+                    <li key={index} className="list-group-item d-flex justify-content-between align-items-center">{item[0].toString()} <span className="badge">{item[1].toString()}</span> </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
     );
   }
 }
